89123ff24f78044f11ab5c9ec9c1779f88be20b9
Bicen Ye <bicenye@Bicens-MacBook-Pro.local>
2018-06-06 21:26:35 -0700

    initial structure working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Vegieburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Beefburger', false);
+
+ 
